51bf84dd2d370e55d1222f36efd8990b8785f154
gildatapuy-20 <131310500+gildatapuy-20@users.noreply.github.com>
2023-06-15 02:36:23 +0000

    TC6_Funcion

--- restenz.py
+++ restenz.py
@@ -1,8 +1,15 @@
 def ecor1(seq, enzima):
-    sitios_corte = []
-    secuencia = seq 
+  """
+  Función ecor1 realiza cortes e imprime los fragmentos
+  input: 
+  secuencia : Secuencia de ADN o ARN.
+  output: 
+  sitios_corte: Lista de fragmentos extraídos de la secuencia y longitud de las mismas
+  """
+  sitios_corte = []
+  secuencia = seq 
     
-    while True:
+  while True:
         sitiocorte = secuencia.find(enzima)
         if sitiocorte == -1:
             break
@@ -10,7 +17,7 @@ def ecor1(seq, enzima):
         frag2 = secuencia[sitiocorte + len(enzima):]
         sitios_corte.append((frag1, len(frag1)))
         secuencia = frag2
-    sitios_corte.append((secuencia, len(secuencia))) 
+  sitios_corte.append((secuencia, len(secuencia))) 
 
-    return ([sitios_corte])
+  return ([sitios_corte])
 
